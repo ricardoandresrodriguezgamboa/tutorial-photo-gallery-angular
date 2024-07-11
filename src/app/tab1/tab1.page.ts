ca130b259f8aa537cb5e14beaea03c90eaeecb59
Ricardo Rodriguez G <ricardoandres749@gmail.com>
2024-07-11 16:44:57 -0400

    nuevo reproductor

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,20 @@
 import { Component } from "@angular/core";
 import { ActionSheetController, MenuController } from "@ionic/angular";
-
+import Hls from 'hls.js';
 @Component({
   selector: "app-tab1",
   templateUrl: "tab1.page.html",
   styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
+
+  private hls: Hls | null = null;
+  private videoElement: HTMLVideoElement | null = null;
+
   constructor(public actionSheetController: ActionSheetController,private menu: MenuController) {}
 
   ionViewDidEnter() {
-    const iframe = document.getElementById('topnewradiovideo') as HTMLIFrameElement;
-    if (iframe) {
-      iframe.src = 'https://www.topnewradio.cl/videos_app/topnewradiovideo.html';
-    }
-
+    this.loadVideo();
 
     this.menuCtrl();
  
@@ -31,13 +31,62 @@ export class Tab1Page {
   }
 
   ionViewDidLeave(){
-    const iframe = document.getElementById('topnewradiovideo') as HTMLIFrameElement;
-    if (iframe) {
-      iframe.src = '';
+    this.cleanupHls();
+  }
+ 
+  loadVideo() {
+    const video = document.getElementById('topnewradiovideo') as HTMLVideoElement;
+    const videoSrc = 'https://panel.tvstream.cl:1936/8052/8052/playlist.m3u8';
+
+    if (Hls.isSupported()) {
+      this.hls = new Hls({
+        liveDurationInfinity: true,
+        enableWorker: true,
+        lowLatencyMode: true,
+      });
+      this.hls.loadSource(videoSrc);
+      this.hls.attachMedia(video);
+      this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play();
+      });
+      this.hls.on(Hls.Events.ERROR, function (event, data) {
+        if (data.fatal) {
+          switch(data.type) {
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              // try to recover network error
+              console.error('fatal network error encountered, try to recover');
+              this.hls.startLoad();
+              break;
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              console.error('fatal media error encountered, try to recover');
+              this.hls.recoverMediaError();
+              break;
+            default:
+              // cannot recover
+              this.hls.destroy();
+              break;
+          }
+        }
+      });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = videoSrc;
+      video.addEventListener('loadedmetadata', function () {
+        video.play();
+      });
     }
   }
+ 
 
-
+  cleanupHls() {
+    if (this.hls) {
+      this.hls.destroy();
+      this.hls = null;
+    }
+    if (this.videoElement) {
+      this.videoElement.pause();
+      this.videoElement.src = '';
+    }
+  }
 
   
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { ActionSheetController, MenuController } from "@ionic/angular";
-
+import Hls from 'hls.js';
 
 @Component({
   selector: "app-tab2",
@@ -8,14 +8,17 @@ import { ActionSheetController, MenuController } from "@ionic/angular";
   styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+
+  private hls: Hls | null = null;
+  private videoElement: HTMLVideoElement | null = null;
+
+
+
   constructor(public actionSheetController: ActionSheetController,private menu: MenuController) {}
 
   ionViewDidEnter() {
-    const iframe = document.getElementById('ritmotoplatinovideo') as HTMLIFrameElement;
-    if (iframe) {
-      iframe.src = 'https://www.topnewradio.cl/videos_app/ritmotoplatinovideo.html';
-    }
-
+    this.loadVideo();
+    
     this.menuCtrl();
 
   }
@@ -28,12 +31,62 @@ export class Tab2Page {
   }
 
   ionViewDidLeave(){
-    const iframe = document.getElementById('ritmotoplatinovideo') as HTMLIFrameElement;
-    if (iframe) {
-      iframe.src = '';
+    this.cleanupHls();
+  }
+ 
+  loadVideo() {
+    const video = document.getElementById('ritmotoplatinovideo') as HTMLVideoElement;
+    const videoSrc = 'https://panel.tvstream.cl:1936/8048/8048/playlist.m3u8';
+
+    if (Hls.isSupported()) {
+      this.hls = new Hls({
+        liveDurationInfinity: true,
+        enableWorker: true,
+        lowLatencyMode: true,
+      });
+      this.hls.loadSource(videoSrc);
+      this.hls.attachMedia(video);
+      this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play();
+      });
+      this.hls.on(Hls.Events.ERROR, function (event, data) {
+        if (data.fatal) {
+          switch(data.type) {
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              // try to recover network error
+              console.error('fatal network error encountered, try to recover');
+              this.hls.startLoad();
+              break;
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              console.error('fatal media error encountered, try to recover');
+              this.hls.recoverMediaError();
+              break;
+            default:
+              // cannot recover
+              this.hls.destroy();
+              break;
+          }
+        }
+      });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = videoSrc;
+      video.addEventListener('loadedmetadata', function () {
+        video.play();
+      });
     }
   }
+ 
 
+  cleanupHls() {
+    if (this.hls) {
+      this.hls.destroy();
+      this.hls = null;
+    }
+    if (this.videoElement) {
+      this.videoElement.pause();
+      this.videoElement.src = '';
+    }
+  }
 
   
 }
